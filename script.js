a9b633d27ded528935ee166d38fb273bd6059888
Bruna Freitas <86448529+freitasbru@users.noreply.github.com>
2021-07-18 22:19:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit' , (e) {
+e.preventDefault()
+    let nome= document.getElementById('nome').value;
+    let email=document.getElementById('email').value;
+    console.log(nome,email);
+
+
+
+})
